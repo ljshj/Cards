ef51dfe9d498ca416bff4271b1ef881ab6cd7dab
唐友亮 <tangyl@enveesoft.com>
2014-03-07 12:30:55 +0800

    完成城市选择功能

--- ButlerCard/Controllers/MyProperty/HXTSelectARegionFirstLevelViewController.m
+++ ButlerCard/Controllers/MyProperty/HXTSelectARegionFirstLevelViewController.m
@@ -8,10 +8,16 @@
 
 #import "HXTSelectARegionFirstLevelViewController.h"
 
+typedef NS_ENUM(NSUInteger, sectionType) {
+    sectionTypeCurrentCity = 0,
+    sectionTypeTopCities,
+    sectionTypeProvinces
+};
+
 @interface HXTSelectARegionFirstLevelViewController ()
-@property (strong, nonatomic) NSString *currentLocation;
+@property (strong, nonatomic) NSString     *currentCity;
+@property (strong, nonatomic) NSArray      *topCities;
 @property (strong, nonatomic) NSDictionary *provinces;
-@property (strong, nonatomic) NSArray  *topCities;
 @end
 
 @implementation HXTSelectARegionFirstLevelViewController
@@ -35,8 +41,9 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    _topCities = [[NSArray alloc] initWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"hotCities" ofType:@"plist"]];
-    _provinces =   [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"provinces" ofType:@"plist"]];
+    _currentCity = @"成都";
+    _topCities   = [[NSArray alloc] initWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"hotCities" ofType:@"plist"]];
+    _provinces   = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"provinces" ofType:@"plist"]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,38 +62,87 @@
     [super viewWillDisappear:animated];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([segue.identifier isEqualToString:@"SelectARegionSecondLevelIdentifier"]) {
-        NSLog(@"############# segue SelectARegionSecondLevelIdentifier");
-    }
-}
-
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 1;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return _provinces.allKeys.count;
+    if (section == sectionTypeCurrentCity) {
+        return 1;
+    } else if (section == sectionTypeTopCities) {
+        return _topCities.count;
+    } else if (section == sectionTypeProvinces) {
+        return _provinces.allKeys.count;
+    } else {
+        NSLog(@"####Error%s %s %d %@", __FILE__, __FUNCTION__, __LINE__, @"Wrong section");
+        return 0;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == sectionTypeCurrentCity) {
+        return @"当前位置";
+    } else if (section == sectionTypeTopCities) {
+        return @"热门城市";
+    } else if (section == sectionTypeProvinces) {
+        return @"按省份选择城市";
+    } else {
+        NSLog(@"####Error%s %s %d %@", __FILE__, __FUNCTION__, __LINE__, @"Wrong section");
+        return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"ProvinceCellIdentifier";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    if (indexPath.section == sectionTypeCurrentCity) {
+        static NSString *CellIdentifier = @"CurrentCityCellIdentifier";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        
+        // Configure the cell...
+        
+        cell.textLabel.text = _currentCity;
+        return cell;
+    } else if (indexPath.section == sectionTypeTopCities) {
+        static NSString *CellIdentifier = @"TopCityCellIdentifier";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        
+        // Configure the cell...
+        
+        cell.textLabel.text = _topCities[indexPath.row];
+        return cell;
+    } else if (indexPath.section == sectionTypeProvinces) {
+        static NSString *CellIdentifier = @"ProvinceCellIdentifier";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        
+        // Configure the cell...
+        
+        NSString *key = [NSString stringWithFormat:@"%d", indexPath.row];
+        NSDictionary *province = _provinces[key];
+        cell.textLabel.text = province.allKeys[0];
+        return cell;
+    } else {
+        NSLog(@"####Error%s %s %d %@", __FILE__, __FUNCTION__, __LINE__, @"Wrong indexPath.section");
+        return nil;
+    }
     
-    NSString *key = [NSString stringWithFormat:@"%d", indexPath.row];
-    NSDictionary *province = _provinces[key];
-    cell.textLabel.text = province.allKeys[0];
-    return cell;
 }
+
+#pragma mark - Table view delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 35;
+}
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+//    return 50;
+//}
 
 /*
 // Override to support conditional editing of the table view.
@@ -127,7 +183,7 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a story board-based application, you will often want to do a little preparation before navigation
@@ -135,8 +191,17 @@
 {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"SelectARegionSecondLevelIdentifier"]) {
+        NSLog(@"############# segue SelectARegionSecondLevelIdentifier");
+        NSIndexPath *selectedIndexPath = [self.tableView indexPathForSelectedRow];
+        if (selectedIndexPath.section == sectionTypeProvinces) {
+            UIViewController *selectARegionSecondLevelViewController = segue.destinationViewController;
+            NSString *key = [NSString stringWithFormat:@"%d", selectedIndexPath.row];
+            NSDictionary *provinceWithIndex = _provinces[key];
+            [selectARegionSecondLevelViewController setValue:provinceWithIndex[provinceWithIndex.allKeys[0]] forKey:@"province"];
+        }
+    }
 }
 
- */
 
 @end
---
+++ ButlerCard/Controllers/MyProperty/HXTSelectARegionSecondLevelViewController.h
@@ -0,0 +1,15 @@
+//
+//  HXTSelectARegionSecondLevelViewController.h
+//  ButlerCard
+//
+//  Created by johnny tang on 3/7/14.
+//  Copyright (c) 2014 johnny tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXTSelectARegionSecondLevelViewController : UITableViewController
+
+@property (strong, nonatomic) NSDictionary *province;
+
+@end
