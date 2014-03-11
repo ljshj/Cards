c8c0f04103cbddf63a59cd12527d0ded973e6322
唐友亮 <tangyl@enveesoft.com>
2014-03-11 11:11:29 +0800

    添加账单详情单元Model层文件

---
+++ ButlerCard/Models/MyProperty/HXTPropertyBillDetailCell.h
@@ -0,0 +1,13 @@
+//
+//  HXTPropertyBillDetailCell.h
+//  ButlerCard
+//
+//  Created by johnny tang on 3/11/14.
+//  Copyright (c) 2014 johnny tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXTPropertyBillDetailCell : NSObject
+
+@end
