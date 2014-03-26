a4fe2751e444cf760264ec1bf98582cea9e1d2cd
唐友亮 <tangyl@enveesoft.com>
2014-03-26 16:58:46 +0800

    选择住房界面UI初版

--- ButlerCard/ChooseHouse/Controller/HXTChooseHouseViewController.m
+++ ButlerCard/ChooseHouse/Controller/HXTChooseHouseViewController.m
@@ -10,6 +10,8 @@
 
 @interface HXTChooseHouseViewController ()
 
+@property (weak, nonatomic) IBOutlet UIPickerView *housePicker;
+
 @end
 
 @implementation HXTChooseHouseViewController
@@ -35,9 +37,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UIPickerView DataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 3;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return 10;
+}
+
+#pragma mark - UIPickerView Delegate
+
 /*
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+    
+}
+ */
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return [NSString stringWithFormat:@"%lu-%lu", component, row];
+}
+
+/*
+- (NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+    
+}
+ */
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSLog(@"row = %lu, component = %lu", row, component);
+}
+
+
+#pragma mark - UI Actons
+
+- (IBAction)okButtonPresssed:(id)sender {
+}
+
 #pragma mark - Navigation
 
+/*
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -45,5 +93,9 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)backButtonPressed:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 
 @end
