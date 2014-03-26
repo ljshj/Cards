8cc9513c126e11c9ad97ead6a3db6fec2184cf69
Cy <774164@qq.com>
2014-03-26 11:52:21 +0800

    修复了程序重的几个警告

--- ButlerCard/MyButler/CYMyBillDetailCell.m
+++ ButlerCard/MyButler/CYMyBillDetailCell.m
@@ -15,38 +15,6 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-        //设置背景色
-        self.backgroundColor = [UIColor greenColor];
-        //标题图片
-        _titleImageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 10, 60, 60)];
-        [_titleImageView setImage:[UIImage imageNamed:@"home_panda"]];
-        _titleImageView.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_titleImageView];
-        //标题lable
-        _titleLable = [[UILabel alloc]initWithFrame:CGRectMake(100, 15, 100, 20)];
-        [_titleLable setText:@"物业费"];
-        _titleLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_titleLable];
-
-        //时间lable
-        _timeLable = [[UILabel alloc]initWithFrame:CGRectMake(100, 45, 100, 20)];
-        [_timeLable setFont:[UIFont systemFontOfSize:12.0f]];
-        [_timeLable setText:@"2014.9.26"];
-        _timeLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_timeLable];
-        //价格lable
-        _moneyLable = [[UILabel alloc]initWithFrame:CGRectMake(180, 30, 100, 20)];
-        [_moneyLable setText:@"729.00"];
-        _moneyLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_moneyLable];
-
-        //状态lable
-        _stateLable = [[UILabel alloc]initWithFrame:CGRectMake(250, 30, 100, 20)];
-        [_stateLable setFont:[UIFont systemFontOfSize:14.0f]];
-        [_stateLable setText:@"交易成功"];
-        _stateLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_stateLable];
-
     }
     return self;
 }
--- ButlerCard/MyButler/CYMyBillMonthCell.m
+++ ButlerCard/MyButler/CYMyBillMonthCell.m
@@ -16,18 +16,6 @@
     if (self)
     {
         // Initialization code
-        //设置背景色
-        self.backgroundColor = [UIColor yellowColor];
-        //设置时间lable
-        _timeLable = [[UILabel alloc]initWithFrame:CGRectMake(5, 5, 100, 20)];
-        [_timeLable setText:@"本月"];
-        _timeLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_timeLable];
-        //设置支出lable
-        _moneyLable = [[UILabel alloc]initWithFrame:CGRectMake(250, 5, 70, 20)];
-        [_moneyLable setText:@"支出998"];
-        _moneyLable.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_moneyLable];
     }
     return self;
 }
